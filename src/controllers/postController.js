8ef7c537e8a154f236a47e7e1ff2e016445a0956
Arminas Gaizutis <arminkeef@gmail.com>
2022-06-03 22:04:52 +0300

    adjustemnts to routes, get users posts by id added

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,15 +1,23 @@
 const asyncHandler = require('express-async-handler');
 
 const Post = require('../models/postModel');
+const User = require('../models/userModel');
 
+//GET ALL POSTS
 const getAllPosts = asyncHandler(async (req, res) => {
   const posts = await Post.find();
 
   res.status(200).json(posts);
 });
-// const getPost = (req, res) => {
-//   res.status(200).json({ message: 'GET POST' });
-// };
+
+// GET MY POSTS
+const getMyPosts = asyncHandler(async (req, res) => {
+  const posts = await Post.find({ user: req.user.id });
+
+  res.status(200).json(posts);
+});
+
+// CREATE POST
 const createPost = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
@@ -19,6 +27,7 @@ const createPost = asyncHandler(async (req, res) => {
   const post = await Post.create({
     text: req.body.text,
     img: req.body.img,
+    user: req.user.id,
   });
 
   res.status(200).json(post);
@@ -30,6 +39,17 @@ const updatePost = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error('Post not found');
   }
+  // Checking for user
+  if (!req.user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+  // Making sure the logged in user matches the post user
+  if (post.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('User not authorized');
+  }
+
   const updatedPost = await Post.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
   });
@@ -37,12 +57,24 @@ const updatePost = asyncHandler(async (req, res) => {
   res.status(200).json(updatedPost);
 });
 
+//DELETE POST
 const deletePost = asyncHandler(async (req, res) => {
   const post = await Post.findById(req.params.id);
   if (!post) {
     res.status(400);
     throw new Error('Post not found');
   }
+  // Checking for user
+  if (!req.user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+  // Making sure the logged in user matches the post user
+  if (post.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('User not authorized');
+  }
+
   await post.remove();
 
   res.status(200).json({ id: req.params.id });
@@ -50,6 +82,7 @@ const deletePost = asyncHandler(async (req, res) => {
 
 module.exports = {
   getAllPosts,
+  getMyPosts,
   createPost,
   updatePost,
   deletePost,
--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -5,11 +5,13 @@ const {
   createPost,
   updatePost,
   deletePost,
+  getMyPosts,
 } = require('../controllers/postController');
 
 const { protect } = require('../middleware/authMiddleware');
 
-router.route('/').get(protect, getAllPosts).post(protect, createPost);
+router.route('/').get(protect, getMyPosts).post(protect, createPost);
+router.route('/memorybox').get(protect, getAllPosts);
 router.route('/:id').put(protect, updatePost).delete(protect, deletePost);
 
 module.exports = router;
